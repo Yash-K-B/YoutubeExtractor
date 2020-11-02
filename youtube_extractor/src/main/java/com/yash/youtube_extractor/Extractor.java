01c1c7fe267d665d3b74bac6b267cb88c714e26a
Yash-K-B <yashwantkumar027@gmail.com>
2020-11-02 15:05:53 +0530

    new HTML page retriever

--- youtube_extractor/src/main/java/com/yash/youtube_extractor/Extractor.java
+++ youtube_extractor/src/main/java/com/yash/youtube_extractor/Extractor.java
@@ -42,10 +42,8 @@ public class Extractor {
         try {
             long start,end;
             start = SystemClock.currentThreadTimeMillis();
-            Connection.Response response = Jsoup.connect(url)
-//                    .cookie("PHPSESSID", cookieID)
-                    .execute();
-            Document doc = Jsoup.parse(response.body());
+            String html = getString(url);
+            Document doc = Jsoup.parse(html);
             String decodeFunctionName = "";
             context = Context.enter();
             scope = context.initStandardObjects();
@@ -130,7 +128,7 @@ public class Extractor {
         try {
             URL webUrl = new URL(url);
             HttpURLConnection connection = (HttpURLConnection) webUrl.openConnection();
-            connection.getDoInput();
+            connection.setRequestProperty("User-Agent", "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.95 Safari/537.11");
             BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
             int numChar;
             char[] buffer = new char[131072];
